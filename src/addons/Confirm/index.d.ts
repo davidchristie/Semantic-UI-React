7a138f0c1a1dbc9e0b82518138f8f687b96578e1
Alexander Fedyashov <alexander.mcgarret@gmail.com>
2017-02-05 11:38:43 +0200

    style(Confirm): refactor, update typings and propTypes usage

--- src/addons/Confirm/index.d.ts
+++ src/addons/Confirm/index.d.ts
@@ -1,26 +1,35 @@
 import * as React from 'react';
 
 interface ConfirmProps {
-
-  /** The cancel button text */
+  /** The cancel button text. */
   cancelButton?: string;
 
-  /** The OK button text */
+  /** The OK button text. */
   confirmButton?: string;
 
   /** The ModalContent text. */
-  content?: string;
+  content?: any;
 
-  /** The ModalHeader text */
-  header?: string;
+  /** The ModalHeader text. */
+  header?: any;
 
-  /** Called when the Cancel button is clicked */
-  onCancel?: () => void;
+  /**
+   * Called when the Cancel button is clicked.
+   *
+   * @param {SyntheticEvent} event - React's original SyntheticEvent.
+   * @param {object} data - All props.
+   */
+  onCancel?: (event: React.MouseEvent<HTMLAnchorElement>, data: ConfirmProps) => void;
 
-  /** Called when the OK button is clicked */
-  onConfirm?: () => void;
+  /**
+   * Called when the OK button is clicked.
+   *
+   * @param {SyntheticEvent} event - React's original SyntheticEvent.
+   * @param {object} data - All props.
+   */
+  onConfirm?: (event: React.MouseEvent<HTMLAnchorElement>, data: ConfirmProps) => void;
 
-  /** Whether or not the modal is visible */
+  /** Whether or not the modal is visible. */
   open?: boolean;
 }
 
